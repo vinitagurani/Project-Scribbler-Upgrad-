1da97f6fe7075f97d624551017d3ea37380daf27
vinita gurnani <vinitagurnani6@gmail.com>
2024-03-19 21:53:19 +0530

    header function in postlist page

--- scripts/common.js
+++ scripts/common.js
@@ -71,3 +71,26 @@ create.addEventListener('click',()=>{
 span2.onclick = function() {
   post.classList.remove('active');
 }
+
+//create button
+var post = document.querySelector(".post-popup");
+
+// Get the button that opens the modal
+var create = document.getElementById("create-post");
+
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("close-post")[0];
+
+// When the user clicks on the button, open the modal
+// function openPop() {
+//   modal.style.visibility = "visible";
+// }
+create.addEventListener('click',()=>{
+  document.querySelector('.active')?.classList.remove('.active');
+  post.classList.add('active');
+});
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+  post.classList.remove('active');
+}
